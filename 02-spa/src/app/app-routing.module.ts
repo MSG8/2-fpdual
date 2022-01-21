fd12b57ee8c5418bea9342db416e52430f906a59
Jorge Perez <jpfernandez098@gmail.com>
2022-01-21 09:00:22 +0100

    home

--- 02-spa/src/app/app-routing.module.ts
+++ 02-spa/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component'
+import { AboutComponent } from './component/about/about.component'
+import { HeroesComponent } from './component/heroes/heroes.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',component: AboutComponent
+  },
+  { path: 'heroes',component: HeroesComponent},
+
+  { path: 'home', component: HomeComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
